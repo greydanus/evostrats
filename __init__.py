0724a6bcfcf018e60c5693b7a10225c186439b6a
Sam <greydanus@users.noreply.github.com>
2020-03-30 20:47:41 -0700

    Add QuadraticModel

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # Sam Greydanus
 
 from .utils import *
-from .models import SimpleCNN
+from .models import SimpleCNN, QuadraticModel
 from .train_mnist import train_mnist
 from .train_quadratic import train_quadratic
 from .grad_estimators import Backprop, Evostrat
