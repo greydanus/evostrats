f6128630e1550c55922035c47fd4329823d38feb
Sam <greydanus@users.noreply.github.com>
2020-08-04 11:29:09 -0700

    Logging fix

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ class QuadraticModel(torch.nn.Module):
   def __init__(self, x):
     super(QuadraticModel, self).__init__()
     self.x = torch.nn.Parameter(x)
+    print("Initialized QuadraticModel with {} parameters".format(count_params(self)))
 
   def forward(self, A):
     return A @ self.x
@@ -24,7 +25,7 @@ class MNISTModel(torch.nn.Module):
     self.conv2 = nn.Conv2d(channels, channels, kernel_size, padding=2)
     self.linear_input_size = H = channels*7*7
     self.linear1 = nn.Linear(H, output_size)
-    print("Initialized SimpleCNN model with {} parameters".format(count_params(self)))
+    print("Initialized MNISTModel with {} parameters".format(count_params(self)))
 
   def forward(self, x):
     x = F.max_pool2d(self.conv1(x).relu(), 2)
